6719871b607d9d6582f8d6216bba758fc3db5166
palaych <palaychm@yandex.ru>
2021-01-11 17:06:44 +0000

    perseus: Define wide color display props
    Change-Id: I7cdce6ea26b935779768eda17732b80a225904d6

--- device.mk
+++ device.mk
@@ -55,6 +55,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.target.rc
 
+# Display
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm845
