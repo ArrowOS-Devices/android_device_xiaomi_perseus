678b9b7495acbeb0528bb2d2864e3555ed3034b3
palaych <palaychm@yandex.ru>
2021-08-26 08:00:14 +0900

    perseus: Use Pixel offline charger animation
    Change-Id: I5d06cc09cb6ad375ddf86334aac8d38e5c714b97

--- arrow_perseus.mk
+++ arrow_perseus.mk
@@ -10,6 +10,7 @@ $(call inherit-product, device/xiaomi/perseus/device.mk)
 $(call inherit-product, vendor/arrow/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := arrow_perseus
